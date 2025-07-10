70529f8f494cb32ed390953471ff334f1f925904
vanaalmagazine <vanaal.artdepartment@gmail.com>
2025-07-09 19:05:58 -0500

    P

--- js/menu.js
+++
@@ -1,6 +0,0 @@
-function showmenu(){
-    document.getElementById("mostrarmenu").classList.toggle("show");
-    document.getElementById("redes_sociales1").classList.toggle("redes_sociales1_scroll");
-    document.getElementById("centrarlogotipo").classList.toggle("centrarlogotipo_scroll");
-}
-
