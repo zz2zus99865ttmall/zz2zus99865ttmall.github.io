85b815371040448bac777b2a49ce4184ef8e1bd9
vanaalmagazine <vanaal.artdepartment@gmail.com>
2025-07-09 19:08:07 -0500

    P

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+function showmenu(){
+    document.getElementById("mostrarmenu").classList.toggle("show");
+    document.getElementById("redes_sociales1").classList.toggle("redes_sociales1_scroll");
+    document.getElementById("centrarlogotipo").classList.toggle("centrarlogotipo_scroll");
+}
+
---
+++ js/scroll_index.js
@@ -0,0 +1,239 @@
+window.sr = ScrollReveal();
+    //Index
+
+    sr.reveal('.conttxtbanner h2', {
+        duration: 2000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.conttxtbanner p', {
+        duration: 4000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.conttxtbanner2 h2', {
+        duration: 2000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.conttxtbanner2 p', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+
+    
+    sr.reveal('.txt_imagenfija_1 h2', {
+        duration: 2000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.txt_imagenfija_1 p', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    });
+    
+
+
+    sr.reveal('.conttxtbanner3 h2', {
+        duration: 2000,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.conttxtbanner3 p', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+
+    sr.reveal('.conttxtbanner4 h2', {
+        duration: 2000,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.conttxtbanner4 p', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.cont_txt_imgfija3 h2', {
+        duration: 2000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.cont_txt_imgfija3 p', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.grid_cont_imgfija_3 #img_fija_2', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.grid_cont_imgfija_3 #img_fija_4', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    /*Atmospheric*/
+
+    sr.reveal('.cont_carrusel_scroll', {
+        duration: 2000,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.textocarrusel', {
+        duration: 2000,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.textocarrusel p', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+
+    /*Editorial*/
+
+    sr.reveal('.conttxtbanner_editorial h2', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.conttxtbanner_editorial p', {
+        duration: 2000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.conttxtbanner2_editorial h2', {
+        duration: 2000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.conttxtbanner2_editorial p', {
+        duration: 2500,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.grid_cont_imgfija_2_editorial #img1_gridfijo2_editorial', {
+        duration: 2500,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.grid_cont_imgfija_2_editorial #img2_gridfijo2_editorial', {
+        duration: 2500,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+
+    /*Architecture*/
+
+    sr.reveal('.txt_inicio_architecture h2', {
+        duration: 2000,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.txt_inicio_architecture p', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.imagen_banner_architecture', {
+        duration: 2000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.txt_banner_architecture h2', {
+        duration: 2000,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.txt_banner_architecture p', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.txt_architecture h2', {
+        duration: 2000,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.txt_architecture p', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.txt_architecture #txt_achitecture_p', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-100px'
+    });
+
+    sr.reveal('.img__architecture img', {
+        duration: 2000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.grid_imagen_architecture img', {
+        duration: 2000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    /**About*/
+    sr.reveal('.imagenartistas', {
+        duration: 2000,
+        origin: 'right',
+        distance: '-100px'
+    });
+    
+    sr.reveal('.txtartistas', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    //contacto
+    sr.reveal('.contacto', {
+        duration: 2000,
+        origin: 'right',
+        distance: '-100px'
+    });    
+
+
+
+
+
+
